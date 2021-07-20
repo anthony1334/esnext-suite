91e072c6d7e0ccd673c805fa41f4894a4bd842c7
Anthony Stephan <66592261+anthony1334@users.noreply.github.com>
2021-07-20 15:17:10 +0200

    add service

--- parkingService.js
+++ parkingService.js
@@ -3,14 +3,14 @@ const request = require("request-promise-native");
 /**
  * structure de la classe Service
  */
- class ParkingService{
-  getParking(id){
+class ParkingService {
+    getParking(id) {
+        return request('http://localhost:8090/parkings/' + id);
+    }
 
-  }
-
-  getReservation(id){
-      
-  }
+    getReservation(id) {
+        return request(`http://localhost:8090/parkings/${id}/reservations`);
+    }
 
 }
 
--- promesse.js
+++ promesse.js
@@ -4,20 +4,17 @@
 
 const request = require("request-promise-native");
 
-function getParkingId(id)
-{
+function getParkingId(id) {
     // la méthode retourne un objet promesse
-    return new Promise((resolve,reject)=>{
-        request('http://localhost:8090/parkings/'+id,{},(err,response) =>
-        {
+    return new Promise((resolve, reject) => {
+        request('http://localhost:8090/parkings/' + id, {}, (err, response) => {
             // callback(id); plus d'utilisation de callback
             // gestion des erreurs
-            if(err){
+            if (err) {
                 reject(err);
                 // en cas d'erreur
-        }
-            else
-            {
+            }
+            else {
                 resolve(JSON.parse(response.body));
                 // en cas de succès
             }
@@ -25,46 +22,24 @@ function getParkingId(id)
     });
 }
 
-/**
- * structure de la classe Service
- */
-class getParkingIdService{
-    getReservationsParkingIdPromise(id)
-{
-    // la méthode retourne un objet promesse
-    return request(`http://localhost:8090/parkings/${id}/reservations`);
-        
-}
 
-}
 
-class  getReservationsParkingIdService{
-    getReservationsParkingIdPromise(id)
-{
-    // la méthode retourne un objet promesse
-    return request(`http://localhost:8090/parkings/${id}/reservations`);
-        
-}
-}
 
 /**
  * 
  *methode qui retourne une promesse de reservation pour un parking donné
  */
-function getReservationsParkingId(id)
-{
+function getReservationsParkingId(id) {
     // la méthode retourne un objet promesse
-    return new Promise((resolve,reject)=>{
-        request(`http://localhost:8090/parkings/${id}/reservations`,{},(err,response) =>
-        {
+    return new Promise((resolve, reject) => {
+        request(`http://localhost:8090/parkings/${id}/reservations`, {}, (err, response) => {
             // callback(id); plus d'utilisation de callback
             // gestion des erreurs
-            if(err){
+            if (err) {
                 reject(err);
                 // en cas d'erreur
-        }
-            else
-            {
+            }
+            else {
                 resolve(JSON.parse(response.body));
                 // en cas de succès
             }
@@ -72,24 +47,23 @@ function getReservationsParkingId(id)
     });
 }
 
-function getReservationsParkingIdPromise(id)
-{
+function getReservationsParkingIdPromise(id) {
     // la méthode retourne un objet promesse
     return request(`http://localhost:8090/parkings/${id}/reservations`);
-        
+
 }
 
 getParkingIdPromise(1).then(
     (data) => {
         console.log(data)
 
-    return getReservationsParkingId(data.id) //retourne un promise
-   }
-    ).then( // Excécute le contenu du promise
-            (data) => 
-            console.log(data)).catch(err=>console.log(err));
-            
-            
+        return getReservationsParkingId(data.id) //retourne un promise
+    }
+).then( // Excécute le contenu du promise
+    (data) =>
+        console.log(data)).catch(err => console.log(err));
+
+
 
 /*Promise.all(
     [getParkingId(1),getReservationsParkingId(1),getParkingId1]
